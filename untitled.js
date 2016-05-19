ffb159cb05d1f10ca83f1d78e805017cafd6f08d
tang7q1 <tang7q1@hotmail.com>
2016-05-19 19:19:49 +0800

    sth. has been corrected finish drawing map and the ‘hero’

--- untitled.js
+++ untitled.js
@@ -1,34 +1,29 @@
 //********定义区******************
 
-$(document).ready(function () { 
-var level=0;
-var monster={
+
+level=0;
+monster={
 	x:[],
 	y:[]
 
 };
-var hero={
-	pic:null,
+hero={
 	x:1,
 	y:1
 };
-var keynumber={};
-var hp=100;
-var power=0;
-var protect=0;
-var App = {
-    c : null,
-    cxt : null
-};
-var map = [[1,1,1,1,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,1,1,1,1],[],[],[],[]];
-	App.c=document.getElementById("mainmap");
-	App.cxt =App.c.getContext("2d"); 
-	drawmap();
+keynumber={};
+hp=100;
+power=0;
+protect=0;
+map = [[1,1,1,1,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,1,1,1,1],[],[],[],[]];
+$(document).ready(function () { 
 	var testt=map[level];
 	for(var i=0;i<5;i++)
   		for(var j=0;j<5;j++)
 		
-	console.log(testt[i*5+j]);});
+	console.log(testt[i*5+j]);
+drawmap();
+});
 //**********画地图********************
 /*var bgReady = false;
 var bgImage = new Image();
@@ -37,26 +32,29 @@ bgImage.onload = function () {
 };
 bgImage.src = "images/background.png";*/
 
-var drawmap=function(level){
+var drawmap=function(){
+	 c=document.getElementById("mainmap");
+    cxt =c.getContext("2d"); 
 	var i,j;
 	var mmap=map[level];
 	var wallpic=document.getElementById("wallpic");
   	for(i=0;i<5;i++)
   		for(j=0;j<5;j++)
   		{
-  			if(mmap[i*5+j]==1)App.cxt.drawImage(wallpic, i*60,j*60,60,60);
+  			console.log(mmap[i*5+j]);
+  			if(mmap[i*5+j]==1)cxt.drawImage(wallpic, i*60,j*60,60,60);
   		}
 
 }
 $(document).keydown(function(event){ 
 if(event.keyCode == 37){     goleft();    }//左
 else if (event.keyCode == 39){ goright(); }//右 
-else if (event.keyCode == 38){ goup;      } //上
-else if (event.keyCode == 40){ godown;    } //下
+else if (event.keyCode == 38){ goup();      } //上
+else if (event.keyCode == 40){ godown();    } //下
 }); 
 var goright=function(){
 	var mmap=map[level];
-	if(mmap[hero.x+1][hero.y]==0)
+	if(mmap[hero.y*5+hero.x+1]==0)
 		{
 			hero.x++;
 			heromove();
@@ -65,7 +63,7 @@ var goright=function(){
 	}
 var godown=function(){
 	var mmap=map[level];
-	if(mmap[hero.x][hero.y+1]==0)
+	if(mmap[(hero.y+1)*5+hero.x]==0)
 		{
 			hero.y++;
 			heromove();
@@ -73,7 +71,7 @@ var godown=function(){
 	}
 var goup=function(){
 	var mmap=map[level];
-	if(mmap[hero.x][hero.y-1]==0)
+	if(mmap[(hero.y-1)*5+hero.x]==0)
 		{
 			hero.y--;
 			heromove();
@@ -81,18 +79,19 @@ var goup=function(){
 	}
 var goleft=function(){
 	var mmap=map[level];
-	if(mmap[hero.x-1][hero.y]==0)
+	if(mmap[hero.y*5+hero.x-1]==0)
 		{
 			hero.x--;
 			heromove();
 		}
 	}
 var heromove=function(){
-	App.cxt.clearRect(0,0,600,600);
-	hero.pic=getElementById("heropic");
+	cxt.clearRect(0,0,600,600);
+	 var heropic=new Image();
+	heropic.src="heropic.png"
 	drawmap(level);
 	//drawmosters();
 	//drawitems();
-	App.cxt.drawImage(heropic,hero.x*60,hero.y*60,60,60);
+	cxt.drawImage(heropic,hero.x*60,hero.y*60,60,60);
 
 }
