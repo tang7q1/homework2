e616004d678a82c12e0f11f7647c3168c240d1f4
tang7q1 <tang7q1@hotmail.com>
2016-05-22 19:35:07 +0800

    new functions: pick key attack monsters get power and blabla

--- untitled.js
+++ untitled.js
@@ -1,7 +1,5 @@
 //********定义区******************
-
-
-level=0;
+level=1;
 monster={
 	x:[],
 	y:[]
@@ -9,117 +7,169 @@ monster={
 };
 hero={
 	x:2,
-	y:7
+	y:8
 };
-keynumber={};
+key1num=0;
+key2num=0;
 hp=100;
-power=0;
-protect=0;
-//map = [[1,1,1,1,1,1,0,0,0,1,1,0,0,0,1,1,0,0,0,1,1,1,1,1,1],[],[],[],[]];
+power=12;
+protect=3;
+money=0;
+turnstatus=1;//1下2上3左4右
 $(document).ready(function () { 
-	var testt=map;
-	for(var i=0;i<10;i++)
-  		for(var j=0;j<10;j++)
-		
-	console.log(testt[i][j]);
-drawmap();
-heromove();
+	drawmap();
+	heromove();
 });
 //**********画地图********************
-/*var bgReady = false;
-var bgImage = new Image();
-bgImage.onload = function () {
-	bgReady = true;
-};
-bgImage.src = "images/background.png";*/
-
+changelevel();
 var drawmap=function(){
 	c=document.getElementById("mainmap");
-    cxt =c.getContext("2d"); 
+    cxt = c.getContext("2d"); 
 	var i,j;
-    	var floorpic=document.getElementById("floorpic");
-		var wallpic=document.getElementById("wallpic");
-		var door=document.getElementById("door");
+    var floorpic=document.getElementById("floorpic");
+	var wallpic=document.getElementById("wallpic");
+	var doorclose=document.getElementById("doorclose");
+	var dooropen=document.getElementById("dooropen");
+	var monster1pic=document.getElementById("monster1pic");
+	var key1pic=document.getElementById("key1");
+	var key2pic=document.getElementById("key2");
 	for(i=0;i<10;i++)
   		for(j=0;j<10;j++){
   			cxt.drawImage(floorpic,i*60,j*60,60,60);
-  		}
+  	}
 	
   	for(i=0;i<10;i++)
   		for(j=0;j<10;j++)
   		{
-  			//console.log(map[i][j]);
-  			if(map[i][j]==1)cxt.drawImage(wallpic, j*60,i*60,60,60);
-			if(map[i][j]==2)cxt.drawImage(door, j*60,i*60,60,60);
-			
-  		}     
-	//var mmap=map[level];
-	
-  
-
+  			if(map[i][j]==1)cxt.drawImage(doorclose, j*60,i*60,60,60);
+			if(map[i][j]==88)cxt.drawImage(dooropen, j*60,i*60,60,60);
+			if(map[i][j]==3)cxt.drawImage(wallpic, j*60,i*60,60,60);
+			if(map[i][j]==11)cxt.drawImage(monster1pic, j*60,i*60,60,60);
+			if(map[i][j]==8)cxt.drawImage(key1pic, j*60,i*60,60,60);
+			if(map[i][j]==9)cxt.drawImage(key2pic, j*60,i*60,60,60);
+	}
 }
 $(document).keydown(function(event){ 
-if(event.keyCode == 37){     goleft();    }//左
-else if (event.keyCode == 39){ goright(); }//右 
-else if (event.keyCode == 38){ goup();      } //上
-else if (event.keyCode == 40){ godown();    } //下
+if(event.keyCode == 37){ event.preventDefault();	goleft();}//左
+else if (event.keyCode == 39){ event.preventDefault();	goright();}//右 
+else if (event.keyCode == 38){event.preventDefault();	goup();}//上
+else if (event.keyCode == 40){event.preventDefault();	godown();}//下
 }); 
-var goright=function(){
-	if(map[hero.y][hero.x+1]==0)
-		{
-			hero.x++;
-			heromove();
-		}
-
+function opendoor(){
+	map[py][px]=0;
+	heromove();
+	console.log(key1num,key2num);
+}
+var operation=function(y,x)
+{
+	if(map[y][x]==0)
+	{
+		hero.x=x;
+		hero.y=y;
+		heromove();
 	}
-var godown=function(){
-	if(map[hero.y+1][hero.x]==0)
+	else if(map[y][x]==8||map[y][x]==9)
 		{
-			hero.y++;
-			heromove();
-		}
+			hero.x=x;
+			hero.y=y;
+			pickkey();
+		}	
+	else if((map[y][x]==1/*&&key1num!=0*/)||(map[y][x]==2&&key2num!=0))
+	{
+		hero.x=x;
+		hero.y=y;
+		if(map[hero.y][hero.x]==1)key1num--;
+		if(map[hero.y][hero.x]==2)key2num--;		
+		py=hero.y;
+		px=hero.x;
+		map[hero.y][hero.x]=88;
+		time=setTimeout("opendoor()",80);
+		heromove();
 	}
-var goup=function(){
-	if(map[hero.y-1][hero.x]==0)
+	else if(map[y][x]==13||map[y][x]==14||map[y][x]==15)
+	{
+		hero.x=x;
+		hero.y=y;
+		if(map[hero.y][hero.x]==13)
+			{if(hp+10<=100)hp+=10;
+			else hp=100;
+			}
+		if(map[hero.y][hero.x]==14)power+=5;
+		if(map[hero.y][hero.x]==15)protect+=5;
+		map[y][x]==0;
+		heromove();
+	}
+	else if(map[y][x]==11||map[y][x]==12)
+	{	
+		console.log(hp);
+		var mhp;
+		if(map[y][x]==11)mhp=20;
+		if(map[y][x])==12)mhp=25;
+		var attacks;
+		if(5+Math.floor(power*0.5)<=20){
+		attacks=Math.ceil(mhp/(5+Math.floor(power*0.5)));
+		hp=hp-(5-Math.floor(protect*0.5))*attacks;
+		}
+		else hp=hp-(20-Math.floor(protect*0.5))
+		if(hp<=0)
+			{
+				gameover();
+			}
+		else 
 		{
-			hero.y--;
-			heromove();
-		}
+		hero.x=x;
+		hero.y=y;	
+		py=y;
+		px=x;
+		map[hero.y][hero.x]=811;
+		time=setTimeout("opendoor()",100);
+		heromove();
+		console.log(hp);
+		}	
 	}
+}
 var goleft=function(){
-	if(map[hero.y][hero.x-1]==0)
-		{
-			hero.x--;
-			heromove();
-		}
-	}
+	turnstatus=3;
+	heromove();
+	operation(hero.y,hero.x-1);
+	
+}
+var goright=function(){
+	turnstatus=4;
+	heromove();
+	operation(hero.y,hero.x+1);
+
+}
+var goup=function(){
+	turnstatus=2;
+	heromove();
+	operation(hero.y-1,hero.x);
+}
+var godown=function(){
+	turnstatus=1;
+	heromove();
+	operation(hero.y+1,hero.x);
+}
+
 var heromove=function(){
 	cxt.clearRect(0,0,600,600);
 	drawmap();
-	 var heropic=document.getElementById("heropic");
-	
+	var heropic1=document.getElementById("heropic1");
+	var heropic2=document.getElementById("heropic2");
+	var heropic3=document.getElementById("heropic3");
+	var heropic4=document.getElementById("heropic4");
+	if(turnstatus==1)cxt.drawImage(heropic1,hero.x*60,hero.y*60,60,60);
+	if(turnstatus==2)cxt.drawImage(heropic2,hero.x*60,hero.y*60,60,60);
+	if(turnstatus==3)cxt.drawImage(heropic3,hero.x*60,hero.y*60,60,60);
+	if(turnstatus==4)cxt.drawImage(heropic4,hero.x*60,hero.y*60,60,60);
 	//drawmonsters();
 	//drawitems();
-	cxt.drawImage(heropic,hero.x*60,hero.y*60,60,60);
-
 } 
-var drawmonsters=function()
+var pickkey=function()
 {
-	var i,j;
-	var mmap=map[level];
-	var monsterpic=new Image();
-	monsterpic.src="monsterpic.png";
-  	for(i=0;i<10;i++)
-  		for(j=0;j<10;j++)
-  		{
-  
-  			if(mmap[i*5+j]==7)cxt.drawImage(monsterpic, i*60,j*60,60,60);
-  		}
-
-}
-var drawitems=function()
-{
-	var i,j;
-	var mmap=map[level];
-	
+	if(map[hero.y][hero.x]==8)key1num++;
+	if(map[hero.y][hero.x]==9)key2num++;
+	map[hero.y][hero.x]=0;
+	heromove();
+	console.log(key1num,key2num);
 }
