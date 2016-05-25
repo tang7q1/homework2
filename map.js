646a336cc40baedc90a7ddc2236871c1a4499786
tang7q1 <tang7q1@hotmail.com>
2016-05-25 22:53:49 +0800

    function changed

--- map.js
+++ map.js
@@ -24,14 +24,44 @@ function changelevel(){
 		map[2][1]=map[2][3]=map[2][4]=map[2][5]=map[2][6]=map[3][6]=map[3][7]=map[4][1]=map[4][2]=map[4][4]=map[4][5]=map[4][6]
 		=map[5][6]=map[6][1]=map[6][2]=map[6][3]=map[6][4]=map[6][6]=map[6][8]=map[8][4]=map[8][6]=3;
 
+		map[2][2]=map[3][8]=map[4][3]=map[7][4]=map[7][6]=1;
+		map[6][7]=2;
+		
+		map[1][7]=map[3][2]=map[7][8]=map[8][7]=11;
+		
+		map[7][1]=map[5][2]=map[3][5]=map[1][8]=map[5][8]=8;
+		
+		map[8][8]=9;
+		
+		map[1][1]=10;
+	}
+	else if(level==2)
+	{
+		hero.x=8;
+		hero.y=2;
+		for(j=0;j<10;++j)//shang xia
+		{
+			map[0][j]=3;
+			map[9][j]=3;
+		}
+
+		for(i=0;i<10;++i)//zuo you
+		{
+			map[i][0]=3;
+			map[i][9]=3;
+		}
+		map[2][1]=map[2][3]=map[2][4]=map[2][5]=map[2][6]=map[3][6]=map[3][7]=map[4][1]=map[4][2]=map[4][4]=map[4][5]=map[4][6]
+		=map[5][6]=map[6][1]=map[6][2]=map[6][3]=map[6][4]=map[6][6]=map[6][8]=map[8][4]=map[8][6]=3;
+
 		map[2][2]=map[3][8]=map[4][3]=map[6][7]=map[7][4]=map[7][6]=1;
 		map[1][7]=map[3][2]=map[7][8]=map[8][7]=11;
 		
-		map[7][1]=8;
+		map[7][1]=map[5][2]=map[3][5]=map[1][8]=8;
 		
 		map[8][8]=9;
+		
+		map[1][1]=10;
 	}
-	else if(level==2){}
 	else if(level==3){}
 	else if(level==4){}
 	else if(level==5){}
--- untitled.js
+++ untitled.js
@@ -12,27 +12,29 @@ hero={
 key1num=0;
 key2num=0;
 hp=100;
-power=12;
-protect=3;
+hp2=100;
+var hp3;
+power=0;
+protect=0;
 money=0;
-turnstatus=1;//1下2上3左4右
-$(document).ready(function () { 
-	drawmap();
-	heromove();
-});
+
+
 //**********画地图********************
 changelevel();
 var drawmap=function(){
 	c=document.getElementById("mainmap");
     cxt = c.getContext("2d"); 
 	var i,j;
-    var floorpic=document.getElementById("floorpic");
+	var floorpic=document.getElementById("floorpic");
 	var wallpic=document.getElementById("wallpic");
-	var doorclose=document.getElementById("doorclose");
+	var door1=document.getElementById("door1");
+	var door2=document.getElementById("door2");
 	var dooropen=document.getElementById("dooropen");
 	var monster1pic=document.getElementById("monster1pic");
 	var key1pic=document.getElementById("key1");
 	var key2pic=document.getElementById("key2");
+	var flag=document.getElementById("flag");
+	var tomb=document.getElementById("tomb");
 	for(i=0;i<10;i++)
   		for(j=0;j<10;j++){
   			cxt.drawImage(floorpic,i*60,j*60,60,60);
@@ -41,25 +43,72 @@ var drawmap=function(){
   	for(i=0;i<10;i++)
   		for(j=0;j<10;j++)
   		{
-  			if(map[i][j]==1)cxt.drawImage(doorclose, j*60,i*60,60,60);
-			if(map[i][j]==88)cxt.drawImage(dooropen, j*60,i*60,60,60);
+  			if(map[i][j]==1)cxt.drawImage(door1, j*60,i*60,60,60);
+			if(map[i][j]==2)cxt.drawImage(door2, j*60,i*60,60,60);
 			if(map[i][j]==3)cxt.drawImage(wallpic, j*60,i*60,60,60);
 			if(map[i][j]==11)cxt.drawImage(monster1pic, j*60,i*60,60,60);
 			if(map[i][j]==8)cxt.drawImage(key1pic, j*60,i*60,60,60);
 			if(map[i][j]==9)cxt.drawImage(key2pic, j*60,i*60,60,60);
+			if(map[i][j]==10)cxt.drawImage(flag, j*60,i*60,60,60);
+			if(map[i][j]==13)cxt.drawImage(tomb, j*60,i*60,60,60);
+			if(map[i][j]==15)cxt.drawImage(dooropen, j*60,i*60,60,60);
+
 	}
 }
 $(document).keydown(function(event){ 
-if(event.keyCode == 37){ event.preventDefault();	goleft();}//左
-else if (event.keyCode == 39){ event.preventDefault();	goright();}//右 
+if(event.keyCode == 37){event.preventDefault();	goleft();}//左
+else if (event.keyCode == 39){event.preventDefault();	goright();}//右 
 else if (event.keyCode == 38){event.preventDefault();	goup();}//上
 else if (event.keyCode == 40){event.preventDefault();	godown();}//下
 }); 
+
+function changekey(){
+	document.getElementById("keybg1").innerHTML=key1num;
+	document.getElementById("keybg2").innerHTML=key2num;
+}
+
+function changehp(){
+	document.getElementById("hp").innerHTML="生命值~~~~ "+hp;
+	document.getElementById("hp2").innerHTML="生命值~~~~ "+hp2;
+	document.getElementById("hp3").innerHTML="敌人生命值~~~ "+hp3;
+}
+
+function changepower(){
+	document.getElementById("power").innerHTML="能力值~~~~ "+power;
+	document.getElementById("power2").innerHTML="能力值~~~~ "+power2;
+}
+
+function changeprotect(){
+	document.getElementById("protect").innerHTML="防御力~~~~ "+protect;
+	document.getElementById("protect2").innerHTML="防御力~~~~ "+protect2;
+}
+
+function changemoney(){
+	document.getElementById("money").innerHTML="财富值~~~~ "+money;
+	document.getElementById("money2").innerHTML="财富值~~~~ "+money2;
+}
+
+var pickkey=function()
+{
+	if(map[hero.y][hero.x]==8)key1num++;
+	else if(map[hero.y][hero.x]==9)key2num++;
+	map[hero.y][hero.x]=0;
+	changekey();
+	heromove();
+}
+
 function opendoor(){
 	map[py][px]=0;
+	changekey();
 	heromove();
-	console.log(key1num,key2num);
 }
+
+function gameover(){
+	
+
+
+}
+
 var operation=function(y,x)
 {
 	if(map[y][x]==0)
@@ -69,64 +118,70 @@ var operation=function(y,x)
 		heromove();
 	}
 	else if(map[y][x]==8||map[y][x]==9)
-		{
-			hero.x=x;
-			hero.y=y;
-			pickkey();
-		}	
-	else if((map[y][x]==1/*&&key1num!=0*/)||(map[y][x]==2&&key2num!=0))
 	{
 		hero.x=x;
 		hero.y=y;
-		if(map[hero.y][hero.x]==1)key1num--;
-		if(map[hero.y][hero.x]==2)key2num--;		
-		py=hero.y;
-		px=hero.x;
-		map[hero.y][hero.x]=88;
-		time=setTimeout("opendoor()",80);
-		heromove();
-	}
-	else if(map[y][x]==13||map[y][x]==14||map[y][x]==15)
+		pickkey();
+	}	
+	else if((map[y][x]==1&&key1num!=0)||(map[y][x]==2&&key2num!=0))
 	{
-		hero.x=x;
-		hero.y=y;
-		if(map[hero.y][hero.x]==13)
-			{if(hp+10<=100)hp+=10;
-			else hp=100;
-			}
-		if(map[hero.y][hero.x]==14)power+=5;
-		if(map[hero.y][hero.x]==15)protect+=5;
-		map[y][x]==0;
-		heromove();
-	}
-	else if(map[y][x]==11||map[y][x]==12)
-	{	
-		console.log(hp);
-		var mhp;
-		if(map[y][x]==11)mhp=20;
-		if(map[y][x])==12)mhp=25;
-		var attacks;
-		if(5+Math.floor(power*0.5)<=20){
-		attacks=Math.ceil(mhp/(5+Math.floor(power*0.5)));
-		hp=hp-(5-Math.floor(protect*0.5))*attacks;
-		}
-		else hp=hp-(20-Math.floor(protect*0.5))
-		if(hp<=0)
-			{
-				gameover();
-			}
-		else 
-		{
-		hero.x=x;
-		hero.y=y;	
+		if(map[y][x]==1)key1num--;
+		if(map[y][x]==2)key2num--;		
 		py=y;
 		px=x;
-		map[hero.y][hero.x]=811;
-		time=setTimeout("opendoor()",100);
+		map[y][x]=15;
 		heromove();
-		console.log(hp);
-		}	
+		time=setTimeout("opendoor()",100);
 	}
+	else if(map[y][x]==11||map[y][x]==12)
+	{
+		var mhp;
+		if(map[y][x]==11)mph=20;
+		else if(map[y][x]==12)mph=25;
+		var attacks;
+		if(5+Math.floor(power*0.5)<=20)
+		{
+			attacks=Math.ceil(mph/(5+Math.floor(power*0.5)));
+			hp -= 5-Math.floor(protect*0.5)*attacks;
+		}
+		else
+			hp -= 20-Math.floor(protect*0.5);
+			
+		changehp();	
+		if(hp<=0)
+		{
+			gameover();
+		}
+		else
+		{
+			py=y;
+			px=x;
+			map[y][x]=13;//dead monster
+			heromove();
+			time=setTimeout("opendoor();heromove()",800);
+		}
+	}
+	else if(map[y][x]==10)
+	{
+		//level++;
+		hero.x=x;
+		hero.y=y;
+		heromove();
+		//document.getElementById("showlevel").innerHTML="第 "+level+" 关";
+		//changelevel();
+		showbox();
+	}
+}
+var showbox=function(){
+	document.getElementById("nextlevel").style.visibility="visible";
+}
+var levelnext=function()
+{
+	level++;
+	document.getElementById("showlevel").innerHTML="第 "+level+" 关";
+	document.getElementById("nextlevel").style.visibility="hidden";
+	changelevel();
+	heromove();
 }
 var goleft=function(){
 	turnstatus=3;
@@ -162,14 +217,8 @@ var heromove=function(){
 	if(turnstatus==2)cxt.drawImage(heropic2,hero.x*60,hero.y*60,60,60);
 	if(turnstatus==3)cxt.drawImage(heropic3,hero.x*60,hero.y*60,60,60);
 	if(turnstatus==4)cxt.drawImage(heropic4,hero.x*60,hero.y*60,60,60);
-	//drawmonsters();
-	//drawitems();
 } 
-var pickkey=function()
-{
-	if(map[hero.y][hero.x]==8)key1num++;
-	if(map[hero.y][hero.x]==9)key2num++;
-	map[hero.y][hero.x]=0;
+$(document).ready(function () { 
+	drawmap();
 	heromove();
-	console.log(key1num,key2num);
-}
+});
